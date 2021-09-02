e55ef9d1044dde20211e7e04e5329fda97e392d9
dvsolodov <d.v.solodov@yandex.ru>
2021-09-02 18:27:29 +1000

    Fixed: duplicate posts

--- index.php
+++ index.php
@@ -1,59 +1,83 @@
 <?php
 
-date_default_timezone_set('Europe/Moscow');
+date_default_timezone_set('Europa/Moscow');
 
-$currentHour = date('H');
+$currentHour = intval(date('H'));
 
-if ($currentHour <= 8 && $currentHour >= 22) {
+file_put_contents('logs', 'Запуск - ' . date('Y-m-d H:i:s', time()) . PHP_EOL,
+    FILE_APPEND);// Логирование запуска скрипта
+
+if ($currentHour < 8 || $currentHour > 22) {
     exit();
 }
 
 date_default_timezone_set('UTC');
 
+file_put_contents('logs', 'Работа - ' . date('Y-m-d H:i:s', time()) . PHP_EOL,
+    FILE_APPEND);// Логирование работы скрипта
+
+
 //Настройки конфигурации
-$apiKey = '0000000'; //API Key для YouTube Data API v3
-$chatId = -0000000; //id телеграм-чата
-$telegramBotId = '00000000'; //Токен телеграм-бота
-$channels = 'channels'; //Путь к файлу с URL youtube-каналов
+$apiKey = '00000000000';               //API Key для YouTube Data API v3
+$chatId = -000000000000000;            //id телеграм-чата
+$telegramBotId = '000000000000000000'; //Токен телеграм-бота
+$channels = 'channels';         //Путь к файлу с URL youtube-каналов
+$ignoreList = 'ignoreList.txt'; //Путь к файлу с ID игнорируемых плейлистов
 $posts = 'posts'; //Путь к файлу со списком залитых на телеграмм постов
 $maxResults = 10; //Максимальное количество видео в выборке для каждого плейлиста канала
-$period = 36000; //Период времени в секундах от текущего назад, в течении которого видео считается новым
+$period = 86400;  //Период времени в секундах от текущего назад, в течении которого видео считается новым
 //Конец настроек конфигурации
 
-$channelUrlsList = file($channels);
-$channelIds = [];
-$playListIds = [];
+$channelUrlsList = file($channels);     // Список URL youtube-каналов
+$channelIds = [];                       // Массив с ID каналов
+$playListIds = [];                      // Массив с ID плейлистов каналов
+$ignorePlayListIds = file($ignoreList); // ID игнорируемых плейлистов
 
 foreach ($channelUrlsList as $channelUrl) {
     $channelId = getChannelIdFromUrl($channelUrl);
     $playListIds = getPlayListIdsByChannelId($channelId, $apiKey);
 
     foreach ($playListIds as $playListId) {
-        $playList = getVideosDataByPlayListId($playListId, $apiKey, $maxResults);
 
-        foreach ($playList as $videoId => $item) {
-            if ($item['title'] == "Private video") {
-                continue;
-            }
+        if (array_search($playListId . PHP_EOL, $ignorePlayListIds) === false) {
 
-            if ((time() - $period) <= strtotime($item['pubDate'])) {
-                $recordElems = [
-                    $item['pubDate'],
-                    $item['channelId'],
-                    $item['title'],
-                    $videoId,
-                ];
-                $record = implode('|', $recordElems) . PHP_EOL;
-                $postsList = file($posts);
+            $playList = getVideosDataByPlayListId($playListId, $apiKey, $maxResults);
 
-                if (array_search($record, $postsList) === false) {
+            foreach ($playList as $videoId => $item) {
+
+                if ($item['title'] == "Private video") {
+                    continue;
+                }
+
+                if ((time() - $period) <= strtotime($item['pubDate'])) {
+                    $recordElems = [
+                        $item['pubDate'],
+                        $item['channelId'],
+                        $item['title'],
+                        $videoId,
+                    ];
+                    $record = implode('|', $recordElems) . PHP_EOL;
+                    $postsList = file($posts);
+
+                    foreach ($postsList as $postRecord) {
+                        $elems = explode('|', $postRecord);
+
+                        if (array_search($videoId . PHP_EOL, $elems) === true) {
+                            continue 2;
+                        }
+                    }
+
                     $link = "https://www.youtube.com/watch?v={$videoId}";
                     $data = array('chat_id' => $chatId, 'text' => $item['title'] . PHP_EOL . PHP_EOL . $link);
 
-                    $sendMsg = file_get_contents('https://api.telegram.org/bot'. $telegramBotId . '/sendMessage?'. http_build_query($data));
+                    $sendMsg =
+                        file_get_contents('https://api.telegram.org/bot' . $telegramBotId . '/sendMessage?' .
+                            http_build_query($data));
 
                     if ($sendMsg) {
                         file_put_contents($posts, $record, FILE_APPEND);
+                        file_put_contents('logs', 'Размещение поста в телеграм-канале - ' . $record . PHP_EOL,
+                            FILE_APPEND);// Логирование публикации поста в телеграм-канале
                     }
                 }
             }
@@ -91,7 +115,8 @@ function getPlayListIdsByChannelId(string $channelId, string $apiKey): array
 //Получить данные на все видео одного плейлиста по его ID
 function getVideosDataByPlayListId(string $playListId, string $apiKey, int $maxResults): array
 {
-    $apiUrl = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={$playListId}&maxResults={$maxResults}&key={$apiKey}";
+    $apiUrl =
+        "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={$playListId}&maxResults={$maxResults}&key={$apiKey}";
     $videos = json_decode(file_get_contents($apiUrl), true);
     $videosData = [];
 
